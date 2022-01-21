88d7d418bd69ef642be88018685946d56e9c3b33
hoangtn <hoangtn0109@gmail.com>
2022-01-21 15:09:27 +0700

    first commit
    Signed-off-by: hoangtn <hoangtn0109@gmail.com>

---
+++ src/test/java/com/tnh/blog/BlogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnh.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
